634a14a3ffb87aefd1ac97e06f0a0490f120ba07
CV-Student <63215608+Cool-CoCoder@users.noreply.github.com>
2024-03-10 15:29:59 +0800

    add test file

---
+++ test.py
@@ -0,0 +1,31 @@
+from pyVHR.analysis.pipeline import Pipeline
+from pyVHR.plot.visualize import *
+from pyVHR.utils.errors import getErrors, printErrors, displayErrors
+
+# params
+wsize = 6                  # window size in seconds
+roi_approach = 'patches'   # 'holistic' or 'patches'
+bpm_est = 'clustering'     # BPM final estimate, if patches choose 'medians' or 'clustering'
+method = 'cpu_CHROM'       # one of the methods implemented in pyVHR
+
+# run
+pipe = Pipeline()          # object to execute the pipeline
+bvps, timesES, bpmES = pipe.run_on_video('test.mp4',
+                                        winsize=wsize,
+                                        roi_method='convexhull',
+                                        roi_approach=roi_approach,
+                                        method=method,
+                                        estimate=bpm_est,
+                                        patch_size=4,
+                                        RGB_LOW_HIGH_TH=(5,230),
+                                        Skin_LOW_HIGH_TH=(5,230),
+                                        pre_filt=True,
+                                        post_filt=True,
+                                        cuda=True,
+                                        verb=True)
+
+print(bvps)
+# # ERRORS
+# RMSE, MAE, MAX, PCC, CCC, SNR = getErrors(bvps, fps, bpmES, bpmGT, timesES, timesGT)
+# printErrors(RMSE, MAE, MAX, PCC, CCC, SNR)
+# displayErrors(bpmES, bpmGT, timesES, timesGT)
